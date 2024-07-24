704c53d09962e19d3e9e37f1f960f7a7f4a1bbb4
Kai Gao <73616322+kaigao-lanl@users.noreply.github.com>
2024-07-24 11:58:13 -0600

    Initial commit

--- example/fatt/run.sh
+++ example/fatt/run.sh
@@ -1,16 +1,21 @@
 
 bindir=$HOME/src/latte/bin
 
-#make clean
-#make
-#./exec
+make clean
+make
+
+# generate model and geometry
+./exec
 
 export OMP_NUM_THREADS=4
 
+# forward modeling
 mpirun -np 12 $bindir/x_eikonal2 param_eikonal.rb
 
+# ad fatt
 mpirun -np 12 $bindir/x_fatt2 param_fatt_ad.rb
 
+# dd fatt
 mpirun -np 12 $bindir/x_fatt2 param_fatt_dd.rb
 
 

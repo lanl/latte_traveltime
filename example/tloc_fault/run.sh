db9dea6fd0534a19bcf971fc38240b90a7260d72
Kai Gao <73616322+kaigao-lanl@users.noreply.github.com>
2024-07-24 11:52:48 -0600

    Initial commit

--- example/tloc_fault/run.sh
+++ example/tloc_fault/run.sh
@@ -1,5 +1,19 @@
 bindir=$HOME/src/latte/bin
 
+make clean
+make
+
+# generate model and geometry
+./exec1
+
+# forward modeling
 mpirun -np 40 $bindir/x_eikonal3 param_eikonal.rb
+
+# add noise
+./exec2
+
+# tloc without regularization
 mpirun -np 40 $bindir/x_tloc3 param_tloc.rb
+
+# tloc with regularization
 mpirun -np 40 $bindir/x_tloc3 param_tloc_reg.rb

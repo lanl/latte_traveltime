5160b78eb76f891c8a37845bb0c5ac32bff88b7e
Kai Gao <73616322+kaigao-lanl@users.noreply.github.com>
2024-07-24 11:56:12 -0600

    Initial commit

--- example/fatt_mask/run.sh
+++ example/fatt_mask/run.sh
@@ -1,16 +1,19 @@
 
 bindir=$HOME/src/latte/bin
 
-#make clean
-#make
-#./exec
+make clean
+make
+
+# generate model and geometry
+./exec
 
 export OMP_NUM_THREADS=1
 
-$HOME/intel/mpi/bin/mpirun -np 40 $bindir/x_eikonal2 param_eikonal.rb
-$HOME/intel/mpi/bin/mpirun -np 40 $bindir/x_fatt2 param_fatt.rb
-$HOME/intel/mpi/bin/mpirun -np 40 $bindir/x_fatt2 param_fatt_dd.rb
+# fatt
+mpirun -np 40 $bindir/x_eikonal2 param_eikonal.rb
+mpirun -np 40 $bindir/x_fatt2 param_fatt.rb
+mpirun -np 40 $bindir/x_fatt2 param_fatt_dd.rb
 
-
-$HOME/intel/mpi/bin/mpirun -np 40 $bindir/x_eikonal2 param_eikonal_refl.rb
-$HOME/intel/mpi/bin/mpirun -np 40 $bindir/x_trtt2 param_trtt.rb
+# reflection tomography
+mpirun -np 40 $bindir/x_eikonal2 param_eikonal_refl.rb
+mpirun -np 40 $bindir/x_trtt2 param_trtt.rb
